0ecf1b399c86fa05ce4abca61e43eef777836129
Wayfarerdesert <125356747+Wayfarerdesert@users.noreply.github.com>
2024-02-07 00:12:20 +0100

    Web Portfolio is ready

---
+++ README.md
@@ -0,0 +1,96 @@
+## Table of Contents
+
+1. [General Info](#general-info)
+2. [Technologies](#technologies)
+3. [Collaboration](#collaboration)
+4. [FAQs](#faqs)
+
+### General Info
+
+---
+
+Welcome to my portfolio website! This is a showcase of my skills, projects, and achievements.
+My portfolio website combines modern design, and responsiveness to showcase my skills and accomplishments effectively.
+
+The website is designed to adapt seamlessly to various screen sizes, ensuring a consistent and user-friendly experience across devices, including desktops, tablets, and mobile phones.
+It incorporates modern and minimalistic design principles, with clean and elegant aesthetics. It provides an intuitive and visually pleasing interface for visitors to explore.
+
+The website also includes a dedicated contact section, allowing visitors to reach out to me directly. It provides contact details and a contact form, making it convenient for potential clients or collaborators, feel free to explore and get in touch with any inquiries or opportunities.
+
+### Screenshot
+
+---
+
+![WebHome](./assets/img/screenshot/home.png)
+
+![WebHomeDark](./assets/img/screenshot//homeDark.png)
+
+![WebHome](./assets/img/screenshot/contact.png)
+
+## Technologies
+
+---
+
+A list of technologies used within the project:
+
+- [HTML](https://www.w3.org/html/): Version 5
+- [CSS](https://www.w3.org/Style/CSS/): Version 4
+- [JavaScript](https://en.wikipedia.org/wiki/JavaScript)
+- [Bootstrap](https://getbootstrap.com/): Version 5.3.0
+- [jQuery](https://jquery.com/): Version 3.6.4
+- [Flexbox](https://example.com): Version 1234
+
+The use of HTML5, CSS3, JavaScript, Bootstrap, jQuery, and Flexbox enables a seamless and engaging experience for visitors.
+
+## Collaboration
+
+---
+
+> "Coming together is a beginning; keeping together is progress; working together is success."
+> **Henry Ford**
+
+On this proyect, i had the pleasure of collaborating with [Oscar Burgos](https://www.linkedin.com/in/oscar-burgos-pacheco/), a talented and experienced web developer.I am extremely grateful for Oscar's contributions and expertise in making this project a success.
+
+If you think we can collaborate on any project, feel free to [conntact me](mailto:facundosantana.tm@gmail.com).
+
+## FAQs
+
+---
+
+1. **What technologies and programming languages are you proficient in?**
+
+   As a web developer, I have expertise in a range of technologies and programming languages.
+
+   - _Frontend_:
+
+     - JavaScript: I have knowledge of JavaScript and its various frameworks and libraries, including jQuery.
+     - HTML: I am well-versed in HTML (including HTML5) and can assist with structuring web content.
+     - CSS: I can help with CSS styling and layout to create visually appealing web pages.
+     - Bootstrap: I am familiar with Bootstrap and can assist in creating responsive and mobile-friendly designs.
+
+   - _Backend_:
+
+     - Python: I have a strong understanding of Python, including its syntax, data structures, and commonly used libraries.
+     - Django: I am proficient in Django.
+     - NumPy: Additionally, I have experience working with libraries like NumPy.
+
+   - _Databases_:
+
+     I have proficiency in MySQL, NoSQL, and MariaDB. These database systems enable me to store and retrieve data efficiently, ensuring the smooth functioning of your web applications.
+
+     - MySQL
+     - NoSQL
+     - MariaDB
+
+2. **How can I contact you for potential collaborations or job opportunities?**
+
+   You can contact me by mail to: facundosantana.tm@gmail.com
+   .
+
+3. **Are you available for freelance work?**
+
+   Yes
+
+4. **Are you open to working with clients remotely or do you prefer on-site work?**
+
+   I am open to work both, remotely and on-site.
---
+++ assets/js/main.js
@@ -0,0 +1,473 @@
+// HEADER TRANSPARENT / SOLID
+var navBar = document.querySelectorAll('#top')[0];
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= 100) {
+        navBar.classList.add('headerSolidBg');
+    } else if (window.screenY < 100) {
+        navBar.classList.remove('headerSolidBg');
+    }
+});
+
+
+// *HAMBURGER BUTTON, APPEARS ON SMALL SCREENS +++++++++++++++++++++++++++
+const hamburgerButton = document.getElementById('hamburger');
+const navList = document.getElementById('navList');
+const config = document.getElementById('config');
+
+function toggButton() {
+    navList.classList.toggle('show');
+    config.classList.toggle('show');
+
+    // Select and remove the span element within the #config div
+    const spanElement = config.querySelector('.languages span');
+    if (spanElement && config.classList == 'show') {
+        spanElement.remove();
+    }
+}
+
+hamburgerButton.addEventListener('click', toggButton);
+
+// Hamburger button changes to X when clicked
+const hamburgerBtn = document.getElementById('hamburger');
+hamburgerBtn.addEventListener('click', function () {
+    if (this.classList.contains('open')) {
+        this.innerHTML = '<i class="fas fa-bars"></i>';
+        this.classList.remove('open');
+    } else {
+        this.innerHTML = '<i class="fas fa-times"></i>';
+        this.classList.add('open');
+    }
+});
+
+
+// *SET ACTIVE NAV LINK ON SCROLL ++++++++++++++++++++++++++++++++++++++++++++++++++
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll('#navList a');
+    const sections = document.querySelectorAll('section');
+    const btnTop = document.getElementById('btnTop');
+
+    window.onscroll = function () {
+        // Get the current scroll position
+        const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+
+        // SET ACTIVE CLASS TO THE NAVBAR LINKS
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop - 150;
+            const sectionHeight = section.offsetHeight;
+
+            if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+                navLinks.forEach(link => {
+                    link.classList.remove('active');
+
+                    // Add active class
+                    document.querySelector(`#navList a[href*='#${section.id}']`).classList.add('active');
+                });
+            };
+        });
+
+        //* HIDE THE btnTop ON HOME SCREEN
+        // Check if the scroll position is within the "about" section
+        if (scrollPosition >= document.getElementById('home').offsetTop &&
+            scrollPosition < document.getElementById('home').offsetTop + document.getElementById('home').offsetHeight) {
+            // Hide the button
+            btnTop.style.display = 'none';
+        } else {
+            // Show the button
+            btnTop.style.display = 'block';
+        }
+    };
+});
+
+
+// ANIMATION SHOWS ON - SECTION TITLES ++++++++++++++++++++++++
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        // console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((element) => observer.observe(element));
+
+
+// *THEME BUTTON +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Automatic theme mode with Jquery based on time.
+// $(function () {
+//     var hour = (new Date).getHours();
+
+//     if (hour >= 20 || hour <= 8) {
+//         $('.lightTheme').removeClass('lightTheme').addClass('darkTheme');
+//     } else {
+//         $('.darkTheme').removeClass('darkTheme').addClass('lightTheme');
+//     }
+// });
+
+// Automatic theme mode with Jquery based on browser theme.
+$(function () {
+    // Check if the browser prefers dark mode
+    var prefersDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    if (prefersDarkMode) {
+        $('.lightTheme').removeClass('lightTheme').addClass('darkTheme');
+    } else {
+        $('.darkTheme').removeClass('darkTheme').addClass('lightTheme');
+    }
+});
+
+// Ensure the initial state is set when the page loads
+document.addEventListener('DOMContentLoaded', function () {
+    const body = document.body;
+    const themeButton = document.getElementById('themeButton');
+    const lightThemeLogo = document.querySelector('.logo-imgLight');
+    const darkThemeLogo = document.querySelector('.logo-imgDark');
+
+    // Check the initial state and set the button and logos accordingly
+    if (body.classList.contains('lightTheme')) {
+        themeButton.innerHTML = '<i class="fa-solid fa-moon"></i>';
+        lightThemeLogo.style.display = 'none';
+        darkThemeLogo.style.display = 'block';
+    } else {
+        themeButton.innerHTML = '<i class="fa-solid fa-sun"></i>';
+        lightThemeLogo.style.display = 'block';
+        darkThemeLogo.style.display = 'none';
+    }
+});
+
+function changeTheme() {
+    const body = document.body;
+    body.classList.toggle('lightTheme');
+    body.classList.toggle('darkTheme');
+
+    const themeButton = document.getElementById('themeButton');
+    const lightThemeLogo = document.querySelector('.logo-imgLight');
+    const darkThemeLogo = document.querySelector('.logo-imgDark');
+
+    if (body.classList.contains('lightTheme')) {
+        themeButton.innerHTML = '<i class="fa-solid fa-moon"></i>';
+        lightThemeLogo.style.display = 'none';
+        darkThemeLogo.style.display = 'block';
+    } else {
+        themeButton.innerHTML = '<i class="fa-solid fa-sun"></i>';
+        lightThemeLogo.style.display = 'block';
+        darkThemeLogo.style.display = 'none';
+    }
+}
+
+// *READ JSON FROM JAVASCRIPT IN ABOUT SECTION +++++++++++++++++++++++++++++++++++
+// Dinamically update selected language
+let currentLanguage = "es"; // Set default language
+let projectCards; //the variant is initialized to store the data of each card x project
+
+// Function to change language
+function changeLanguage(language) {
+    if (language !== currentLanguage) {
+        // Remove selectedLang class from all language elements
+        document.querySelectorAll('.languages p').forEach(element => {
+            element.classList.remove('selectedLang');
+        });
+
+        // Add selectedLang class to the clicked language element
+        document.getElementById(language).classList.add('selectedLang');
+
+        currentLanguage = language;
+        // Fetch the language data from the JSON file
+        fetch(`./assets/data/languages/data${language.toUpperCase()}.json`)
+            .then(response => response.json())
+            .then(data => {
+                updateContent(data),
+                    projectCards = data.projects;
+                renderProjectCard(projectCards);
+            })
+            .catch(error => console.error('Error fetching language data:', error));
+    }
+}
+
+//navBar & Titles
+const homeElements = document.querySelectorAll("[data-home]");
+const aboutElements = document.querySelectorAll("[data-about]");
+const portfolioElements = document.querySelectorAll("[data-portfolio]");
+const skillsetElements = document.querySelectorAll("[data-skillset]");
+const contactElements = document.querySelectorAll("[data-contact]");
+
+// Function to update content on the page
+function updateContent(data) {
+    const navBarData = data.navBar[0];
+    // navBar & Titles
+    homeElements.forEach(element => {
+        element.textContent = navBarData.dataHome;
+    });
+
+    aboutElements.forEach(element => {
+        element.textContent = navBarData.dataAbout;
+    });
+
+    portfolioElements.forEach(element => {
+        element.textContent = navBarData.dataPortfolio;
+    });
+
+    skillsetElements.forEach(element => {
+        element.textContent = navBarData.dataSkillset;
+    });
+
+    contactElements.forEach(element => {
+        element.textContent = navBarData.dataContact;
+    });
+
+    // ********************************************
+    //About
+    const aboutData = data.about[0];
+    const resumeLinkElement = document.querySelector("[data-resume]");
+    const resumeLinkElementPhone = document.querySelector("[data-resume-phone]");
+    document.querySelector("[data-presentation]").textContent = aboutData.presentation;
+    document.querySelector("[data-training]").textContent = aboutData.training;
+    document.querySelector("[data-certifications]").textContent = aboutData.certifications;
+
+    // Select Eng or Esp resume link
+    if (aboutData) {
+        const resumeLink = aboutData.link;
+
+        if (resumeLinkElement || resumeLinkElementPhone) {
+            resumeLinkElement.textContent = aboutData.resume;
+            resumeLinkElement.setAttribute("href", resumeLink);
+            resumeLinkElementPhone.textContent = aboutData.resume;
+            resumeLinkElementPhone.setAttribute("href", resumeLink);
+        } else {
+            console.error("Element with data-resume not found");
+        }
+    } else {
+        console.error("Data for about section not found");
+    }
+
+    let educationHTML = "";
+    for (let i = 0; i < data.education.length; i++) {
+        let education = data.education[i];
+        let itemHTML = "<li><strong>" + education.studyType + "</strong>" + " " + "<span>" + education.institution + "</span>" + " " + education.courses + "</li>";
+        educationHTML += itemHTML;
+    }
+    document.querySelector("[data-education]").innerHTML = educationHTML;
+
+    // ********************************************
+    //Contact Form
+    const contactFormData = data.contactForm[0];
+    document.querySelector("[data-name]").textContent = contactFormData.name;
+    document.querySelector("#email").placeholder = contactFormData.placeholder;
+    document.querySelector("[data-affair]").textContent = contactFormData.affair;
+    document.querySelector("[data-message]").textContent = contactFormData.message;
+    document.querySelector("[data-sendButton]").textContent = contactFormData.sendButton;
+
+    // ******************************************
+    // Footer
+    const footerData = data.footer[0];
+    const developedElement = document.querySelector("[data-developed]");
+    if (developedElement) {
+        developedElement.textContent = footerData.developed;
+    } else {
+        console.error("Element with data-developed not found");
+    }
+
+    document.querySelector("[data-developed]").textContent = footerData.developed
+    document.querySelector("[data-rights]").textContent = footerData.rights
+    document.querySelector("[data-made]").textContent = footerData.made
+    document.querySelector("[data-place]").textContent = footerData.place
+}
+
+// Fetch the default language data and set the content when the page loads
+fetch('./assets/data/languages/dataES.json')
+    .then(response => response.json())
+    .then(data => {
+        updateContent(data);
+        projectCards = data.projects;
+        renderProjectCard(projectCards);
+        document.getElementById(currentLanguage).classList.add('selectedLang');
+    })
+    .catch(error => console.error('Error fetching default language data:', error));
+
+
+// * RENDERING OF THE PORTFOLIO CARDS +++++++++++++++++++++++++++++++++++++++++++++++++++++
+const projectCard = document.querySelector("[data-project-card]");
+const projectCardTemplate = document.getElementById("card-template")
+// Function to render the template for each project
+function renderProjectCard(cards) {
+    projectCard.innerHTML = ""
+
+    cards.forEach(card => {
+        const element = projectCardTemplate.content.cloneNode(true)
+
+        element.querySelector("[data-title]").textContent = card.name;
+        element.querySelector("[data-description]").textContent = card.description;
+        element.querySelector("[data-img]").setAttribute("src", `./assets/img/portfolio/${card.img}.webp`);
+
+        const techContainer = element.querySelector("[data-technologies]");
+        card.technologies.forEach(tech => {
+            const icon = document.createElement("i");
+            icon.className = `fa-${tech.type} fa-${tech.name} fa-2x`;
+            techContainer.appendChild(icon)
+        });
+
+        const codeLink = element.querySelector("[data-code]");
+        codeLink.href = card.code;
+
+        const projectLink = element.querySelector("[data-link]");
+        projectLink !== null ? (card.link !== "" ? projectLink.href = card.link : projectLink.style.display = "none") : null;
+
+        projectCard.append(element)
+    })
+}
+
+// Enlarge the size of the img of each card in the center of the screen onclick
+function toggleClickedImg(element) {
+    // Toggle the "clickedImg" class on the clicked image
+    element.classList.toggle("clickedImg");
+}
+
+// *FORM VALIDATION ++++++++++++++++++++++++++++
+// !CHECK FORM VALIDATION is not correct ===================================================
+// document.getElementById('sendButton').addEventListener('submit', function (event) {
+//     event.preventDefault();
+// });
+
+
+// function ValidateEmail(emailId) {
+//     let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.w{2,4})+$/;
+//     if (emailId.value.match(emailRegex)) {
+//         document.form.focus();
+//         return true;
+//     } else {
+//         alert("invalid email address.");
+//         document.form.focus();
+//         return false;
+//     }
+// }
+// !CHECK FORM VALIDATION is not correct ===================================================
+
+const form = document.getElementById("form");
+const inputs = document.querySelectorAll("#form input");
+
+const validation = {
+    name: /^[a-zA-Z]+\s+[a-zA-Z]{2, 20}$/,
+    email: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.w{2,4})+$/
+}
+
+const validateForm = (e) => {
+    switch (e.target.name) {
+        case "name":
+            if (validation.name.test(e.target.value)) {
+                document.getElementById("nameForm").classList.remove("invalidInput");
+            } else {
+                document.getElementById("nameForm").classList.add("validForm");
+            }
+
+            break;
+        case "email":
+
+            break;
+        case "affair":
+
+            break;
+        case "message":
+
+            break;
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener("keyup", validateForm)
+    input.addEventListener("blur", validateForm)
+})
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+})
+
+
+// Replacing the Default Message for Validation
+var inputName = document.getElementById('name');
+inputName.oninvalid = function (event) {
+    event.targetset.setCustomValidity('Ingresa un nombre valido');
+}
+// !CHECK FORM VALIDATION is not correct ===================================================
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// EmailJs +++++++++++++++++++++++++++++++++++++++++++++++++
+function showAlert(message) {
+    const customAlert = document.getElementById('customAlert');
+    const alertMessage = document.getElementById('alertMessage');
+    const closeAlert = document.getElementById('closeAlert');
+
+    alertMessage.textContent = message;
+    customAlert.style.display = 'block';
+
+    // Close the alert when the "Close" button is clicked
+    closeAlert.addEventListener('click', function () {
+        customAlert.style.display = 'none';
+    });
+}
+
+
+const formHtml = document.getElementById('form');
+const btn = document.querySelector("[data-sendButton]")
+const closeAlertText = document.querySelector("[data-closeAlert]")
+
+document.getElementById('form')
+    .addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        if (currentLanguage == "es") {
+            btn.textContent = 'Enviando...';
+            closeAlertText.textContent = 'Cerrar';
+        } else {
+            btn.textContent = 'Sending...';
+            closeAlertText.textContent = 'Close';
+        }
+
+
+        const serviceID = 'default_service';
+        const templateID = 'PortfolioWeb_n38ejdj';
+
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                btn.textContent = 'Send Email';
+                if (currentLanguage == "es") {
+                    btn.textContent = 'Enviar';
+                    showAlert('Enviado correctamente!');
+                } else {
+                    btn.textContent = 'Send';
+                    showAlert('Submitted successfully!');
+                }
+                formHtml.reset();
+
+            }, (err) => {
+                if (currentLanguage == "es") {
+                    alert('Hubo un error, inténtalo mas tarde!');
+                    alert(JSON.stringify(err));
+                } else {
+                    alert('There was an error, try again later!');
+                    alert(JSON.stringify(err));
+                }
+            });
+    });
+
+// *FOOTER +++++++++++++++++++++++++++++++++++++++++++++++++++++
+// CLOCK ++++++++++++++++++++++++++++++++++++++++++++++++++
+const time = document.getElementById('time');
+const date = document.getElementById('date');
+
+const interval = setInterval(() => {
+    const local = new Date();
+    time.innerHTML = local.toLocaleTimeString('en-GB');
+
+}, 1000);
+
+
+// Get current year to display in footer.
+const currentYear = document.getElementById('year');
+const year = new Date().getFullYear();
+currentYear.innerHTML = year;
+
+
+$(document).ready()
+// $load on star
